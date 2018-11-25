bb33d9d68908569f57f8621c36fa0ad910f80ef0
Luis Fernando <luis.city@gmail.com>
2018-11-25 20:19:14 -0200

    adicionado screen Setup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { SwitchNavigator } from 'react-navigation';
 
 import Main from './screens/Main';
 import Home from './screens/Home';
+import Setup from './screens/Setup';
 import PrimeiraScreen from './screens/PrimeiraScreen';
 import SegundaScreen from './screens/SegundaScreen';
 
 const App = SwitchNavigator(
   {
     Main,
+    Setup,
     Home,
     PrimeiraScreen,
     SegundaScreen,
